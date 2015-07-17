fbb0d16fb354ccc2268cad1e1b273222b1a9c72a
Walter Mauritz <walter.mauritz@r-solution.at>
2015-07-17 17:48:13 +0200

    bookmarkable link added

---
+++ myjsf2/src/main/java/at/wm/webapp/action/EmployeeController.java
@@ -0,0 +1,44 @@
+package at.wm.webapp.action;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedProperty;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Scope("view")
+@Component
+public class EmployeeController extends BasePage implements Serializable {
+
+	private static final long serialVersionUID = 6629483801771322695L;
+
+	@ManagedProperty("#{param.id}")
+	private Long id = 1l;
+
+	private Long notaryId;
+
+	public Long getNotaryId() {
+		log.debug("get notaryId: " + notaryId);
+		return notaryId;
+	}
+
+	public void setNotaryId(Long notaryId) {
+		log.debug("set notaryId: " + notaryId);
+		this.notaryId = notaryId;
+	}
+
+	public Long getId() {
+		log.debug("get id: " + id);
+		return id;
+	}
+
+	public void setId(Long id) {
+		log.debug("set id: " + id);
+		this.id = id;
+	}
+
+	public void actionLoad(Long id, Long notaryId) {
+		log.debug(String.format("load data by id: %s, notaryId: %s ///", id, notaryId));
+	}
+}
